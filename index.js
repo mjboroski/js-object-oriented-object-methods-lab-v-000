d1b6bba42fca48d1073299a3bcbd0260a3374596
Michael Boroski <mjboroski@gmail.com>
2018-07-09 07:33:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function BoardMember(name, homeState, training){
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+  this.veto = function{return "No, I must disagree'};
+  this.approve = function{return "You can do that!'};
+  this.doCharity = function{return "I like to help people.'};
+  this.releasePressStatement = function{return "No, I must disagree'};
+  this.sayHi  = function{return "No, I must disagree'};
+}
