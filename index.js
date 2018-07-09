e705b9bbc8a3e46afd8d484b205b1546c515be5f
Michael Boroski <mjboroski@gmail.com>
2018-07-09 07:34:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function BoardMember(name, homeState, training){
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-  this.veto = function{return "No, I must disagree'};
-  this.approve = function{return "You can do that!'};
-  this.doCharity = function{return "I like to help people.'};
-  this.releasePressStatement = function{return "No, I must disagree'};
-  this.sayHi  = function{return "No, I must disagree'};
+  this.veto = function{return "No, I must disagree"'};
+  this.approve = function{return "You can do that!"};
+  this.doCharity = function{return "I like to help people."};
+  this.releasePressStatement = function{return "You will see great things from Scuber."};
+  this.sayHi  = function{return "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>."};
 }
